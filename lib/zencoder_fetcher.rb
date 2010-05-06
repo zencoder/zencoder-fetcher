c44fac7113b2bc7c1646a7e79bb82c1b667c5b03
chriswarren <christopher.warren@gmail.com>
2010-05-06 12:32:47 -0500

    cleaning things up. posting JSON in the body instead of query-string.

--- lib/zencoder_fetcher.rb
+++ lib/zencoder_fetcher.rb
@@ -11,20 +11,24 @@ module ZencoderFetcher
   
   def self.request(options={})
     api_key = options[:api_key]
-    post_uri = options[:url] ? options[:url] : "http://localhost:3000"
+    post_uri = options[:url] ? options[:url] : "http://localhost:3000/"
     per_page = options[:count] ? options[:count] : 50
     page = options[:page] ? options[:page] : 1
     
     begin
       response = HTTParty.get("http://app.zencoder.com/api/notifications.json?api_key=#{api_key}&per_page=#{per_page}&page=#{page}")
-      if response.is_a?(Array)
-        response.each do |job|
-          job = JSON.parse(job)
-          HTTParty.post(post_uri, job)
+      if response.is_a?(String)
+        if response.body =~ /\{"errors"/
+          puts JSON.parse(response.body)["errors"]
+        else
+          i = 0
+          JSON.parse(response).each do |job|
+            options = {:headers => {"Content-type" => "application/json"}, :body => job}
+            HTTParty.post(post_uri, options)
+            i += 1
+          end
+          puts "#{i} notifications retrieved and posted to #{post_uri}"
         end
-        puts "#{response.size} notifications retrieved and posted to #{post_uri}"
-      elsif response.body =~ /errors/
-        puts JSON.parse(response.body)["errors"]
       else
         puts "No notifications found."
       end
