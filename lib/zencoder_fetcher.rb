18ca85e01f3a4d6478ba8e8631344a9ab144ec26
chriswarren <christopher.warren@gmail.com>
2010-05-07 11:12:15 -0500

    added since_id handling and throttling requests to no more than 10 seconds

--- lib/zencoder_fetcher.rb
+++ lib/zencoder_fetcher.rb
@@ -14,20 +14,25 @@ module ZencoderFetcher
     post_uri = options[:url] ? options[:url] : "http://localhost:3000/"
     per_page = options[:count] ? options[:count] : 50
     page = options[:page] ? options[:page] : 1
+    since_job_id = (options[:all] || options[:since_job_id].to_s == "") ? 0 : options[:since_job_id]
     
     begin
-      response = HTTParty.get("http://app.zencoder.com/api/notifications.json?api_key=#{api_key}&per_page=#{per_page}&page=#{page}")
-      if response.is_a?(String)
+      response = HTTParty.get("http://app.zencoder.com/api/notifications.json?api_key=#{api_key}&per_page=#{per_page}&page=#{page}&since_id=#{since_job_id}")
+      
+      if response.body.is_a?(String)
         if response.body =~ /\{"errors"/
           puts JSON.parse(response.body)["errors"]
         else
           i = 0
-          JSON.parse(response).each do |job|
+          latest_job_id = 0
+          JSON.parse(response.body).each do |job|
             options = {:headers => {"Content-type" => "application/json"}, :body => job}
             HTTParty.post(post_uri, options)
+            latest_job_id = JSON.parse(job)["job"]["id"].to_i if JSON.parse(job)["job"]["id"].to_i > latest_job_id
             i += 1
           end
           puts "#{i} notifications retrieved and posted to #{post_uri}"
+          return latest_job_id
         end
       else
         puts "No notifications found."
